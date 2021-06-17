bcfbb5b6ba86090ba87669f4f595d4207811246c
Leonardo Cid <leonardo.cid@nutechnologyinc.com>
2021-06-17 16:51:20 -0500

    Setting up project

---
+++ Devrank_Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Devrank_Exercise
+//
+//  Created by Jose Leonardo Cid Fabila on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
